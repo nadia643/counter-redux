78c809b116a8a63d1714105a4315d05a4fe86602
John Doe <nadia643@gmail.com>
2020-06-22 11:07:43 +0100

    added switch statement to app.js in case of decrease, increase, reset

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import React from "react";
 import Counter from "./Counter";
+import { createStore } from 'redux';
+
+// setup initial state 
+const defaultState = {
+  count: 54,
+  name: "nadia"
+}
+
+// setup reducer
+function reducer(state, action) {
+  // console.log({ state, action})
+  // if(action.type === "DECREASE") {
+    // state.count = state.count -1;
+    // return state;
+    // return {...state, count: state.count - 1}
+//   }
+switch(action.type) {
+  case "DECREASE": 
+    return {...state, count: state.count - 1}
+  case "INCREASE": 
+      return {...state, count: state.count + 1};
+  case "RESET": 
+      return {...state, count: 0};
+
+      default:
+        return state;
+}
+  
+}
+
+// setup store - the 2 arguments are the old state or the state
+// before update, and then is the action (what we want to do)
+const store = createStore(reducer, defaultState);
+
+store.dispatch({ type: "RESET" })
+store.dispatch({ type: "DECREASE" })
+store.dispatch({ type: "INCREASE" })
+store.dispatch({ type: "INCREASE" })
+
+
+console.log(store.getState());
+
 const App = () => {
-  return <Counter />;
+  return <Counter state={store.getState()} />;
 };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 
-function Counter() {
+function Counter(props) {
+  const amount = props.state.count;
+  const name = props.state.name;
   const [count, setCount] = useState(0);
 
   return (
     <div className="container">
       <h1>Counter</h1>
-      <p className="counter">{count}</p>
+      <h2>{name}</h2>
+      <p className="counter">{amount}</p>
       <div className="buttons">
         <button 
           type="button" 
